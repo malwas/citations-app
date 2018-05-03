0fe20e040e83c437631e879998e9f30f6184537b
Malwina Tarnowska <sobmal@gmail.com>
2018-05-04 00:30:05 +0200

    Citations controller added, citations model added

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -1,2 +1,19 @@
 class CitationsController < ApplicationController
+	def index
+		@citations = Citation.all
+	end
+	
+	def show
+		redirect_to action: "index"
+	end
+ 
+	def new
+	end
+	
+	def create
+		@citation = Citation.new(params.require(:citation).permit(:text))
+ 
+		@citation.save
+		redirect_to @citation
+	end
 end
